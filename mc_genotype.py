3cee83874926a000239da4e6ef438f239f107c67
Henk den Bakker <henkcdenbakker@gmail.com>
2017-03-05 13:07:59 -0600

    Changed back mccortex vcfcov parameters; may have to exclude samples that differ by a large number of SNPs/variants

--- mc_genotype.py
+++ mc_genotype.py
@@ -8,7 +8,7 @@ def geno_cov(reference, calls_vcf, raw_ctx, outdir):
     ref = os.path.basename(reference)
     sample = os.path.basename(raw_ctx).rstrip('.ctx')
     subprocess.call(
-        ["mccortex63 vcfcov -m 12G -n 500M -r " + outdir + "/ref/" + ref + " -f -o " + outdir + "/" + sample + ".cov.vcf " +
+        ["mccortex63 vcfcov -m 1G -r " + outdir + "/ref/" + ref + " -f -o " + outdir + "/" + sample + ".cov.vcf " +
          calls_vcf + " " + outdir + "/raw/" + raw_ctx + ".ctx" ], stdout=subprocess.PIPE, shell=True)
 
 def main():
