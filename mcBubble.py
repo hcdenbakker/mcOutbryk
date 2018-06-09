b44228dffcaf079ffede73bdb8486c7689a755b1
Henk den Bakker <henk@CaptainCrunch.CaptainCrunch.griffin.uga.edu>
2018-06-09 10:31:20 -0400

    added functionality for k-mer size

--- mcBubble.py
+++ mcBubble.py
@@ -6,38 +6,39 @@ import subprocess
 from sys import argv
 
 
-def bubble_call(clean_binary, reference_binary, reference, outdir):
+def bubble_call(mccortex, clean_binary, reference_binary, reference, outdir):
     #bubble calls cleaned binaries in parallel and does filtering of resulting vcfs
     bin = os.path.basename(clean_binary)
     ref_bwa = os.path.basename(reference)
-    subprocess.call(["cd " + outdir + "; mccortex63 join -q -m 8G -o " + clean_binary + "_plus_" + reference_binary +" ref/" + reference_binary +
-                     " clean/" + clean_binary], stdout=subprocess.PIPE, shell=True)
-    subprocess.call(["cd " + outdir + "; mccortex63 bubbles -q -m 8G -o " + clean_binary + "_bubbles.txt.gz --haploid 0 -S -f " +
-                     clean_binary + "_plus_" + reference_binary], stdout=subprocess.PIPE, shell=True)
+    #subprocess.call(["cd " + outdir + "; " + mccortex + " join -q -m 8G -o " + clean_binary + "_plus_" + reference_binary +" ref/" + reference_binary +
+    #                 " clean/" + clean_binary], stdout=subprocess.PIPE, shell=True)
+    subprocess.call(["cd " + outdir + "; " + mccortex + " bubbles -q -m 8G -o " + clean_binary + "_bubbles.txt.gz --haploid 0 -S -f ref/" +
+                     reference_binary + " clean/" + clean_binary], stdout=subprocess.PIPE, shell=True)
     subprocess.call(["cd " + outdir + "; bash mccortex_print_flanks.sh " + clean_binary + "_bubbles.txt.gz " +
                      "> " + clean_binary + ".flanks" ], stdout=subprocess.PIPE, shell=True)
     subprocess.call(["cd " + outdir + "; bwa mem ref/" + ref_bwa + " "+ clean_binary + ".flanks > " +
                     clean_binary +".sam"], stdout=subprocess.PIPE, shell=True)
-    subprocess.call(["cd " + outdir + "; mccortex63 calls2vcf -q -F" + clean_binary +".sam -o " + clean_binary +
+    subprocess.call(["cd " + outdir + "; " + mccortex + " calls2vcf -q -F" + clean_binary +".sam -o " + clean_binary +
                      "_bub.vcf " + clean_binary + "_bubbles.txt.gz ref/" + ref_bwa], stdout=subprocess.PIPE, shell=True)
     subprocess.call(["cd " + outdir + "; vcf-sort " + clean_binary + "_bub.vcf | vcfuniq >" + clean_binary + "_bub.uniq.vcf"],
                     stdout=subprocess.PIPE, shell=True)
-    subprocess.call(["cd " + outdir + "; mccortex63 vcfcov -q -n 50M -m 8G --out " + clean_binary + "_bub.cov.vcf --ref ref/" +
+    subprocess.call(["cd " + outdir + "; " + mccortex + " vcfcov -q -n 50M -m 8G --out " + clean_binary + "_bub.cov.vcf --ref ref/" +
                      ref_bwa + " " + clean_binary + "_bub.uniq.vcf clean/" + clean_binary], stdout=subprocess.PIPE, shell=True)
 
     subprocess.call(["cd " + outdir + "; mc_vcf_filter.py " + clean_binary + "_bub.cov.vcf > " + clean_binary +
                      ".final.vcf"], stdout=subprocess.PIPE, shell=True)
-    subprocess.call(["cd " + outdir + "; rm clean/" + clean_binary + " " + clean_binary + "_plus_" + reference_binary + " " +
+    subprocess.call(["cd " + outdir + "; rm clean/" + clean_binary + " " +
                      clean_binary + "_bubbles.txt.gz " + clean_binary + ".flanks " + clean_binary + ".sam " +
                      clean_binary + "_bub.uniq.vcf " + clean_binary + "_bub.vcf " + clean_binary + "_bub.cov.vcf "],
                      stdout=subprocess.PIPE, shell=True)
 
 def main():
-    sample_ctx = argv[1]
-    reference_ctx = argv[2]
-    reference = argv[3]
-    outdir = argv[4]
-    bubble_call(sample_ctx, reference_ctx, reference, outdir)
+    mccortex = argv[1]
+    sample_ctx = argv[2]
+    reference_ctx = argv[3]
+    reference = argv[4]
+    outdir = argv[5]
+    bubble_call(mccortex, sample_ctx, reference_ctx, reference, outdir)
 
 if __name__ == '__main__':
     main()
--- mc_genotype.py
+++ mc_genotype.py
@@ -28,19 +28,20 @@ def remove_dummy(vcf_in, vcf_out):
 
 
 
-def geno_cov(reference, calls_vcf, raw_ctx, outdir):
+def geno_cov(mccortex, reference, calls_vcf, raw_ctx, outdir):
     ref = os.path.basename(reference)
     sample = os.path.basename(raw_ctx).rstrip('.ctx')
     subprocess.call(
-        ["mccortex63 vcfcov -q -n 50M -m 1G -r " + outdir + "/ref/" + ref + " -f -o " + outdir + "/" + sample + ".cov.vcf " +
+        [mccortex + " vcfcov -q -n 50M -m 1G -r " + outdir + "/ref/" + ref + " -f -o " + outdir + "/" + sample + ".cov.vcf " +
          calls_vcf + " " + outdir + "/raw/" + raw_ctx + ".ctx" ], stdout=subprocess.PIPE, shell=True)
 
 def main():
-    reference = argv[1]
-    calls_vcf = argv[2]
-    raw_ctx = argv[3]
-    outdir = argv[4]
-    geno_cov(reference, calls_vcf, raw_ctx, outdir)
+    mccortex = argv[1]
+    reference = argv[2]
+    calls_vcf = argv[3]
+    raw_ctx = argv[4]
+    outdir = argv[5]
+    geno_cov(mccortex, reference, calls_vcf, raw_ctx, outdir)
 
 if __name__ == '__main__':
     main()
